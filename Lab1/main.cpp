fb0b539c28b6506ac21fe56091a1a5ef25e1113e
Ted <persisne@gmail.com>
2020-01-06 23:02:13 +0900

    go

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Add.h"
 
 using namespace lab1;
@@ -5,6 +6,8 @@ using namespace lab1;
 int main()
 {
 	int result = Add(1, 2);
+
+	std::cout << "Hello, " << "WORLD :" << result << std::endl;
 
 	return result;
 }
