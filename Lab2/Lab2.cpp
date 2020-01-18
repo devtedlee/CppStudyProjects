0531449f74e6abee2a1663254aa81ea27e177a48
Ted <persisne@gmail.com>
2020-01-19 00:06:00 +0900

    lab2 - complete local test

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -9,22 +9,28 @@ namespace lab2
 {
     void PrintIntegers(std::istream& in, std::ostream& out)
     {
-        const int OUT_LENGTH = 12;
-        int number;
-        string trash;
+        constexpr int OUT_LENGTH = 12;
 
         out << "         oct        dec      hex" << endl;
         out << "------------ ---------- --------" << endl;
 
         while (true) 
         {
+            int number;
+            string trash;
+
             in >> number;
 
             if (!in.fail())
             {
-                out << setfill(' ') << setw(OUT_LENGTH) << right << showbase << oct << number;
-                out << setfill(' ') << setw(OUT_LENGTH) << right << showbase << dec << number;
-                out << setfill(' ') << setw(OUT_LENGTH) << right << uppercase << noshowbase << hex << number << endl;
+                if (number < 0)
+                {
+                    continue;
+                }
+
+                out << setfill(' ') << setw(OUT_LENGTH) << right << noshowbase << oct << number;
+                out << setw(OUT_LENGTH - 1) << showbase << dec << number;
+                out << setw(OUT_LENGTH - 3) << uppercase << noshowbase << hex << number << endl;
 
                 continue;
             }
@@ -41,23 +47,32 @@ namespace lab2
 
     void PrintMaxFloat(std::istream& in, std::ostream& out)
     {
-        const int OUT_LENGTH = 14;
-        const int FLOAT_PRECISION = 3;
-        float number;
+        constexpr int OUT_LENGTH = 14;
+        constexpr int FLOAT_PRECISION = 3;
         float max_float = NULL;
-        string trash;
         char pos;
 
         while (true)
         {
+            float number;
+            string trash;
+
             in >> number;
 
             if (!in.fail())
             {
-                pos = number >= 0 ? '+' : '-';
+                if (number >= 0)
+                {
+                    pos = '+';
+                }
+                else
+                {
+                    pos = '-';
+                    number = -number;
+                }
 
                 out << setfill(' ') << setw(6) << right << pos;
-                out << setfill(' ') << setw(OUT_LENGTH) << right << showbase << setprecision(FLOAT_PRECISION) << number;
+                out << setw(OUT_LENGTH) << fixed << showbase << setprecision(FLOAT_PRECISION) << number << endl;
                 
                 if (max_float == NULL || number > max_float)
                 {
@@ -76,7 +91,16 @@ namespace lab2
             in >> trash;
         }
 
-        pos = max_float >= 0 ? '+' : '-';
-        out << "max: " << pos << setfill(' ') << setw(OUT_LENGTH) << right << showbase << setprecision(FLOAT_PRECISION) << max_float << endl;
+        if (max_float >= 0)
+        {
+            pos = '+';
+        }
+        else
+        {
+            pos = '-';
+            max_float = -max_float;
+        }
+
+        out << "max: " << pos << setfill(' ') << setw(OUT_LENGTH) << right << fixed << showbase << setprecision(FLOAT_PRECISION) << max_float << endl;
     }
 }
